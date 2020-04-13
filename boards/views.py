47f2615204c94e1949eeb52df2bab6f61bbf04c7
apul1421 <apul.gupta1421@gmail.com>
2020-04-13 19:18:52 +0530

    initial commit

---
+++ boards/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+# Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+    return HttpResponse("Hello World!")
+
