f5cd42dea9dcd9c5aabaacd1f8a5f5757bd2eb5e
apul1421 <apul.gupta1421@gmail.com>
2020-04-15 00:24:39 +0530

    static template

--- boards/views.py
+++ boards/views.py
@@ -4,12 +4,5 @@ from .models import Board
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request,'home.html',{'boards':boards})
 
